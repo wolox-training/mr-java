3ff15a5b38ca419cbd431ca4a09d2738645127b1
michelle <michelle.rabinovich@wolox.com.ar>
2019-04-22 17:38:21 -0300

    useless imports erased

--- training/src/main/java/wolox/training/controllers/BookController.java
+++ training/src/main/java/wolox/training/controllers/BookController.java
@@ -1,17 +1,15 @@
 package wolox.training.controllers;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import wolox.training.models.Book;
-import wolox.training.repositories.BookRepository;
+
 
 @Controller
 public class BookController {
-    
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model){
 
