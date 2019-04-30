6a35b6b30e306956a32596be73d3a4906df2ee1c
michelle <michelle.rabinovich@wolox.com.ar>
2019-04-30 15:03:54 -0300

    PR commit fixed (for real) - BookRepository extends CrudRepository

--- training/src/main/java/wolox/training/repositories/BookRepository.java
+++ training/src/main/java/wolox/training/repositories/BookRepository.java
@@ -2,12 +2,13 @@ package wolox.training.repositories;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Component;
 import wolox.training.models.Book;
 
 @Component
-public interface BookRepository extends Repository<Book, Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 
     public Optional<Book> findFirstByAuthor(String author);
 
