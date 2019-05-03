b708dc8e94164922e36969c4ba8ad0c40c45ad6b
rabinovichm <michu.rabinovich@hotmail.com>
2019-05-03 14:15:18 -0300

    Removed id settet

--- training/src/main/java/wolox/training/models/User.java
+++ training/src/main/java/wolox/training/models/User.java
@@ -45,11 +45,7 @@ public class User {
     public Long getId() {
         return id;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    
     public String getUsername() {
         return username;
     }
