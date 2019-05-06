35186b54891ff959c29066a91bce20e2675a3eab
michelle <michelle.rabinovich@wolox.com.ar>
2019-05-06 15:24:30 -0300

    response status added to exceptions

--- training/src/main/java/wolox/training/exceptions/BookIdMismatchException.java
+++ training/src/main/java/wolox/training/exceptions/BookIdMismatchException.java
@@ -1,10 +1,12 @@
 package wolox.training.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Book Id Mismatch")
 public class BookIdMismatchException extends Exception {
 
-    public BookIdMismatchException(){
-
-    }
+    public BookIdMismatchException(){ super();  }
 
     public BookIdMismatchException(String message, Throwable cause){
         super(message, cause);
--- training/src/main/java/wolox/training/exceptions/UserNotFoundException.java
+++ training/src/main/java/wolox/training/exceptions/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package wolox.training.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User Not Found")
 public class UserNotFoundException extends Exception {
 
     public UserNotFoundException(){
